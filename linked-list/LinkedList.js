c1ec996e6ba628e507ea23561c23cfe9d1ea92c0
Zekumoru Dragonhart <dev@zekumoru.com>
2022-11-29 14:47:21 +0100

    Add stub linked list methods

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -1,3 +1,49 @@
 export default class LinkedList {
+  get size() {
+    throw new Error('Stub!');
+  }
 
+  get head() {
+    throw new Error('Stub!');
+  }
+
+  get tail() {
+    throw new Error('Stub!');
+  }
+
+  append(value) {
+    throw new Error('Stub!');
+  }
+
+  prepend(value) {
+    throw new Error('Stub!');
+  }
+
+  insertAt(value, index) {
+    throw new Error('Stub!');
+  }
+
+  pop() {
+    throw new Error('Stub!');
+  }
+
+  removeAt(index) {
+    throw new Error('Stub!');
+  }
+
+  at(index) {
+    throw new Error('Stub!');
+  }
+
+  contains(value) {
+    throw new Error('Stub!');
+  }
+
+  find(value) {
+    throw new Error('Stub!');
+  }
+
+  toString() {
+    throw new Error('Stub!');
+  }
 }
